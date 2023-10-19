8b5207f0cbe6a452d681e3cf0965fd1e666e4d4d
4112029012_programming <144210274+eason930924@users.noreply.github.com>
2023-10-19 15:21:05 +0800

    Delete 抄/計算機概論HW-week6-2.py

--- 抄/計算機概論HW-week6-2.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.137.1",3000))
-outdata = input("傳送訊息至伺服器:")
-s.send(outdata.encode())
-indata = s.recv(1024)
-print("由伺服器接收:",indata.decode())
-s.close()
