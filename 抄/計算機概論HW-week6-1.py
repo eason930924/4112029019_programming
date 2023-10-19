12f8b73ac32db23fb26022197be80cec4e4a4058
4112029012_programming <144210274+eason930924@users.noreply.github.com>
2023-10-19 15:21:13 +0800

    Delete 抄/計算機概論HW-week6-1.py

--- 抄/計算機概論HW-week6-1.py
+++
@@ -1,9 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.137.1',3000))
-s.listen(5)
-conn,addr = s.accept()
-print("連線至",addr)
-conn.send(conn.recv(1024))
-conn.close()
-s.close()
